de67a825c87b54ec8a64c3b2a18e6f88b2c17cd7
Eduardo Souza de Oliveira <eduardosouza0301@gmail.com>
2025-05-26 20:15:47 -0300

    Publicado
    Código POO Pessoa.java

---
+++ POOPessoa.java
@@ -0,0 +1,11 @@
+public class POOPessoa {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+
+        // Define o nome usando set
+        p.setNome("Maria");
+
+        // Obtém o nome usando get
+        System.out.println(p.getNome());  // Saída no console: Maria
+    }
+}
