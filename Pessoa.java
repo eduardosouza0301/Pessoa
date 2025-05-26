67a72d054ba65694c177dbd01d35c874529aa17f
Eduardo Souza de Oliveira <eduardosouza0301@gmail.com>
2025-05-26 20:13:11 -0300

    Publicado
    Código Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    // Atributo privado
+    private String nome;
+
+    // Método getter - retorna o nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Método setter - define o nome
+    public void setNome(String novoNome) {
+        nome = novoNome;
+    }
+}
